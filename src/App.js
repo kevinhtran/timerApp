4cf133501342018ca5af2bcebdfbd31c3e354099
kevinhtran <kevdeveloper7@gmail.com>
2021-10-25 02:38:07 -0700

    fix typo for variable

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const App = () => {
     // You can use this variable to calculate the difference between the current date and the first day of New Years.
     let year = new Date.getFullYear();
     // When you use the year variable in place of a hard-coded year, you will always have the current year.
-    let difference = +new Date(`01/01/${year}`) + new Date();
+    let difference = +new Date(`01/01/${year}`) - +new Date();
   };
 
   return (
