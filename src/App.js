17817ea7e57c6e79a5a43abeedb02884e30d4a14
kevinhtran <kevdeveloper7@gmail.com>
2021-10-25 04:26:39 -0700

    displaying the time left

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import React, { useState, useEffect } from 'react'
 const App = () => {
 
   const calculateTimeLeft = () => {
-    let year = new Date().getFullYear();
-    let difference = +new Date(`01/01/${year}`) - +new Date();
+    let nextYear = new Date().getFullYear() + 1;
+    let difference = +new Date(`01/01/${nextYear}`) - +new Date();
     let timeLeft = {};
 
     if (difference > 0) {
@@ -21,27 +21,17 @@ const App = () => {
   };
 
   const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
-  // const [year] = useState(new Date().getFullYear());
+  const [nextYear] = useState(new Date().getFullYear() + 1);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setTimeLeft(calculateTimeLeft());
-      // setYear(new Date().getFullYear());
     }, 1000);
     return () => clearTimeout(timer)
   });
 
-  // We will use Object.keys to iterate over the timeLeft object and build out a display component.
-  // You will use the display component to show the time left before the New Year begins.
-
-  // we create a new variable called timerComponents.
   const timerComponents = [];
-  // after iterating over the keys in timeLeft, you will use this variable to push a new JSX component with the time left.
-  // we use Object.keys to iterate over the timeLeft object you returned from your calculateTimeLeft function.
-  // Here the code loops through the properties of the timeLeft object.
-  // If the timer interval has a value above zero, it adds an element to the timerComponents array.
-  // the {" "} in the code is used so that the intervals that display the time left do not run into each other when displayed on the screen.
-  // This should allow you to be ready to add the new JSX in the App components return statement to display the time left until New Year.
+
   Object.keys(timeLeft).forEach((interval) => {
     if (!timeLeft[interval]) {
       return;
@@ -54,8 +44,18 @@ const App = () => {
     );
   });
 
+  // Now we will add several JSX components to the return statement
+  // We use the ternary operator to check if there is time left or if it is time for New Year
+  // To use the timerComponents array, you need to check its length and either return it or let the user know that the timer has already elapsed.
+  // In React JSX components, you use a ternary operator in place of a JS if statement. This is because only expressions are allowed inside JSX.
+  // The timerComponents.length line of code checks to see if there is anything inside the timerComponents array and renders it if there is, otherwise it renders Time's up. Happy New Year!!!
+  // we create a new state for the current year up there under  the timeLeft state variable.
+  // we set the initial state to new Date().getFullYear() + 1 because it's the next year's new year count down.
+  // this method will grab the current year like you used in the calculateTimeLeft function
   return (
     <div>
+      <h1>{nextYear} New Year Countdown</h1>
+      {timerComponents.length ? timerComponents : <span>Time's up. Happy New Year!!!</span>}
     </div>
   )
 }
