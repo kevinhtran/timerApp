b91a5e06de6aa90894ad8ab11d993d2bd4daac20
kevinhtran <kevdeveloper7@gmail.com>
2021-10-25 02:18:50 -0700

    calculated how much time will be left

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
+  // This function will calculate the time remaining between the current date and the first date of New Years.
+  const calculateTimeLeft = () => {
+    // This is a Date object to find the current year but can also use it to work with dates and times.
+    // You can use this variable to calculate the difference between the current date and the first day of New Years.
+    let year = new Date.getFullYear();
+    // When you use the year variable in place of a hard-coded year, you will always have the current year.
+    let difference = +new Date(`01/01/${year}`) + new Date();
+  };
+
   return (
-    <>
-      <div>Hello World!</div>
-    </>
-  );
+    <div>
+    </div>
+  )
 }
 
-export default App;
+export default App
