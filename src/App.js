6765e6c78c81a67632c88fa7e61c4b264e684b00
kevinhtran <kevdeveloper7@gmail.com>
2021-10-25 04:27:01 -0700

    no comments

--- src/App.js
+++ src/App.js
@@ -44,14 +44,6 @@ const App = () => {
     );
   });
 
-  // Now we will add several JSX components to the return statement
-  // We use the ternary operator to check if there is time left or if it is time for New Year
-  // To use the timerComponents array, you need to check its length and either return it or let the user know that the timer has already elapsed.
-  // In React JSX components, you use a ternary operator in place of a JS if statement. This is because only expressions are allowed inside JSX.
-  // The timerComponents.length line of code checks to see if there is anything inside the timerComponents array and renders it if there is, otherwise it renders Time's up. Happy New Year!!!
-  // we create a new state for the current year up there under  the timeLeft state variable.
-  // we set the initial state to new Date().getFullYear() + 1 because it's the next year's new year count down.
-  // this method will grab the current year like you used in the calculateTimeLeft function
   return (
     <div>
       <h1>{nextYear} New Year Countdown</h1>
