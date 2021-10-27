36ce18db6aed5a71b0a08ef382577dc0b260e9b8
kevinhtran <kevdeveloper7@gmail.com>
2021-10-27 01:10:33 -0700

    clear out comments

--- src/Components/CountdownTimer/Countdown.js
+++ src/Components/CountdownTimer/Countdown.js
@@ -4,39 +4,19 @@ import './Clock.css';
 
 const CountDown = (props) => {
 
-  // const [timerHours, setTimerHours] = useState(props.timerData.hours);
-  // const [timerMinutes, setTimerMinutes] = useState(props.timerData.minutes);
-  // const [timerSeconds, setTimerSeconds] = useState(props.timerData.seconds);
-
   const [timerData, setTimerData] = useState(props);
 
   let interval;
 
   const startTimer = () => {
 
-    // const countDownHours = new Date(props.timerData.hours).getTime();
-    // const countDownMinutes = new Date(props.timerData.minutes).getTime();
-    // const countDownSeconds = new Date(props.timerData.seconds).getTime();
-    // const countDownDate = new Date("January 01, 2022").getTime();
-
     interval = setInterval(() => {
 
-      // const now = new Date().getTime();
-      // const distanceHours = countDownHours - now;
-      // const distanceMinutes = countDownMinutes - now;
-      // const distanceSeconds = countDownSeconds - now;
-      // const distanceDate = countDownDate - now;
-
-      // const hours = Math.floor((distanceHours % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
-      // const minutes = Math.floor((distanceMinutes % (60 * 60 * 1000)) / (1000 * 60));
-      // const seconds = Math.floor((distanceSeconds % (60 * 1000)) / 1000);
-      // debugger;
       const hours = timerData.timerData.hours * 60 * 60 * 1000;
       const minutes = timerData.timerData.minutes * 60 * 1000;
       const seconds = timerData.timerData.seconds * 1000;
 
       let totalDistanceSeconds = hours + minutes + seconds
-      // debugger;
 
       let decrementedTimetotalDistanceSeconds = totalDistanceSeconds
       if (decrementedTimetotalDistanceSeconds <= 0) {
@@ -45,17 +25,7 @@ const CountDown = (props) => {
       } else {
         // Update Timer
 
-        // countDownHours(props.timerData.hours);
-        // countDownMinutes(props.timerData.minutes);
-        // countDownSeconds(props.timerData.seconds);
-
-        // let decrementedTimetotalDistanceSeconds = totalDistanceSeconds - 1000
-        // let decrementedTimetotalDistanceSeconds = totalDistanceSeconds
         decrementedTimetotalDistanceSeconds -= 1000;
-
-        // totalDistanceSeconds -= 1000;
-        // debugger;
-
 
         const newHours = Math.floor((decrementedTimetotalDistanceSeconds % (24 * 60 * 60 * 1000)) / (1000 * 60 * 60));
         const newMinutes = Math.floor((decrementedTimetotalDistanceSeconds % (60 * 60 * 1000)) / (1000 * 60));
@@ -63,17 +33,6 @@ const CountDown = (props) => {
         console.log(decrementedTimetotalDistanceSeconds)
         props.handleUpdate(timerData, newHours, newMinutes, newSeconds);
 
-
-        // props.handleUpdate(props, hours, minutes, seconds);
-        // props.handleUpdate(props, hours, minutes, seconds);
-
-        // setTimerHours(hours);
-        // setTimerMinutes(minutes);
-        // setTimerSeconds(seconds);
-
-        // setTimerHours(props.timerData.hours);
-        // setTimerH  ours(props.timerData.minutes);
-        // setTimerHours(props.timerData.seconds);
       }
     }, 1000);
   };
@@ -92,9 +51,6 @@ const CountDown = (props) => {
 
     console.log("use effect should run every re render")
   }, [props]);
-  /**
-   * TimerForm -> 
-   */
 
   return (
     <div className="App">
