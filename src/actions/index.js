8d920f4d260365827813e3729fb7cbe50411e014
Kevin Tran <kurated@Kevins-MBP.T-mobile.com>
2022-01-21 23:45:52 -0800

    notes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,6 @@
+// Why make a variable for each action name?
+// By defining the variable the value appears only in a single location and can easily be changed
+// Exporting the variable form this file allows the code editor to catch errors and prevents us from making spelling mistakes
 export const NEW_TIMER = "NEW_TIMER";
 export const addTimer = (name) => {
   return {
